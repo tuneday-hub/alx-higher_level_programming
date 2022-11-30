72a3f875167c155f587d796f8d3a47a3d80d8c54
tuneday-hub <eltswish@gmail.com>
2022-11-30 07:23:56 +0100

    Completed Task

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
+#for char in range(26):
+# if char != 4 and char != 16:
+# print("{:s}".format(chr(char + ord("a"))), end="")
