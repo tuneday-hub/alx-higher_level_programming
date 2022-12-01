473b4c4e68fd929692ace6aaf86f3b8a5a9779c3
tuneday-hub <eltswish@gmail.com>
2022-12-01 07:15:08 +0100

    Completed Task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,5 @@
 for i in range(100):
     if i % 10 > int(i / 10):
         print("{}{}".format(int(i / 10), i % 10), end="")
-        if (i != i[-1]):
+        if (i != 89):
             print(", ", end="")
-if (i == i[-1]):
-    print("")
