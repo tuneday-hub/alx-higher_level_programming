6f48a4a8ab5f6a858ed05f4d703866acb301749c
tuneday-hub <eltswish@gmail.com>
2022-12-04 17:36:12 +0100

    Completed Task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for list in my_list:
-        if my_list % 2 == 0:
+        if list % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
