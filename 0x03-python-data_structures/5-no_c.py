86ed8ff48017ef28f500e7b499600ae7c9536fb6
tuneday-hub <eltswish@gmail.com>
2022-12-06 07:26:03 +0100

    Completed Task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_dict = {c: None, C: None}
+    my_dict = {ord(c): None, ord(C): None}
     new_string = my_string.translate(my_dict)
     return new_string
