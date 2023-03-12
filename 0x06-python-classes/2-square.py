bc0fa8061be016e31f7dc7814e8ef2f69d12b7bb
tuneday-hub <eltswish@gmail.com>
2023-03-12 14:26:36 +0100

    Completed Task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' A Module that creates a Square object '''
 
-class Square;
-''' Creating an Object template '''
+class Square:
+    ''' Creating an Object template '''
 
     def __init__(self, size = 0):
         '''
