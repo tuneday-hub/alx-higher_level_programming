cd151dcae3109d51fadeb99a4f34eb915a179c47
tuneday-hub <eltswish@gmail.com>
2023-03-14 03:42:00 +0100

    Completed Task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 /*
-   Commandline arguments in js is made possible using the process.argv
-   which is an array that contains all arguments of the CLI.
+   Commandline arguments in js is made
+   possible using the process.argv which is an
+   array that contains all arguments of the CLI.
    
-   process.argv[0] contains the absolute path to the node binary file
+   process.argv[0] contains the absolute
+   path to the node binary file
    
-   process.argv[1] contains the absolute file path to the .js file in
+   process.argv[1] contains the absolute
+   file path to the .js file in
    which the js engine runs.
    
-   so process.argv[2] ... contains users passed in arguments.
+   so process.argv[2] ...
+   contains users passed in arguments.
 */
 if (process.argv.length < 3) {
   console.log('No argument');
