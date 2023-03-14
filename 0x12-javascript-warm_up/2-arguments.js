66f057cef60792a111e0eb3b792cd81a846b4e5c
tuneday-hub <eltswish@gmail.com>
2023-03-14 03:27:13 +0100

    Completed Task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -11,9 +11,9 @@
   so process.argv[2] ... contains users passed in arguments.
 */
 if (process.argv.length < 3) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length == 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
