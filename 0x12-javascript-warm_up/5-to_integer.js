b9a9b5e7aa984bcefe90229c0d8cb8e66810b2e9
tuneday-hub <eltswish@gmail.com>
2023-03-14 05:39:52 +0100

    Completed Task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const value = process.argv[2];
+const value = parseInt(process.argv[2]);
 if (isNaN(value)) {
   console.log('Not a number');
 } else {
-  console.log('My number:', value | 0);
+  console.log('My number: ' + value);
 }
