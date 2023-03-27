ca2223df92c5c645bef2a05535e011bafd5dba19
tuneday-hub <eltswish@gmail.com>
2023-03-27 11:32:47 +0100

    Completed Task

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,8 +40,7 @@ class Rectangle:
         """Returns the area of the rectangle"""
         return (self.__width * self.__height)
     def perimeter(self):
-        """Returns the perimeter of the rectangle
-           when width or height is not equal to 0."""
+        """Returns the perimeter of the rectangle"""
            if self.__width == 0 or self.__height == 0:
                return (0)
            return (2 * (self.__width + self.__height))
